46397d80f35bb2b32d0933e794aaa406d58944aa
Davide <[davidemastronardi92@gmail.com]>
2023-04-03 17:41:29 +0200

    wip

--- src/App.js
+++ src/App.js
@@ -10,13 +10,20 @@ import {
   useLocation,
 } from "react-router-dom";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Registrazione from "./pages/Registrazione";
+
 
 function App() {
   return (
     <div className="App">
       <Router>
+        
         <Routes>
-          <Route element={<Home />} path="/"></Route>
+
+          <Route element={<Login />} path="/"></Route>
+          <Route element={<Registrazione />} path="/registrazione"></Route>
+          <Route element={<Home />} path="/home"></Route>
         </Routes>
       </Router>
     </div>
